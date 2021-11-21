1cb735ba17a2dcda578477b8ad7b1d0b3838fa7e
emilyzengers <69126128+emilyzengers@users.noreply.github.com>
2021-11-21 12:37:50 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,11 +4,23 @@
 # Don't forget to add data folder for your data
 # Replace the following line with real application code
 
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### task ii
+
+# In[8]:
+
+
 # import libraries
 
 import psycopg2
 
 import pandas as pd
+
+
+# In[24]:
+
 
 # question 1
 # List the x players who achieved highest improvement across all skillsets
@@ -29,6 +41,10 @@ def most_improvement(x):
     # check to make sure x is an integer
     if isinstance(x, int) is not True:
         return("Must input integer")
+    
+    # check to make sure x is not zero or negative
+    if x <= 0:
+        return "The number of players to display must be a postive integer"
     
     # select long name, overall, and sofifa (unique) id from players20
     # select sofifa id and overall from players19
@@ -55,7 +71,11 @@ def most_improvement(x):
     most_improved = df3.values.tolist()
     return most_improved
 
-most_improvement(10)
+most_improvement(1.2)
+
+
+# In[25]:
+
 
 # question 2
 # What are the y clubs that have largest number of players with contracts ending in 2021?
@@ -64,6 +84,10 @@ def num_clubs(y):
     # check to make sure y is an integer
     if isinstance(y, int) is not True:
         return("Must input integer")
+    
+    # check to make sure y is not zero or negative
+    if y <= 0:
+        return "The number of clubs to display must be a postive integer"
     
     conn = psycopg2.connect(host="localhost", 
                     port="5432", 
@@ -85,7 +109,11 @@ def num_clubs(y):
     cur.close()
     conn.close()
 
-num_clubs(10)
+num_clubs(3)
+
+
+# In[20]:
+
 
 # question 3
 # List the z clubs with largest number of players in the dataset where z >= 5
@@ -94,6 +122,14 @@ def num_clubs_lim(z):
     # z has to be at least 5, z >=5
     if z < 5:
         return "The number of clubs to display should at least be 5"
+    
+    # check to make sure z is an integer
+    if isinstance(z, int) is not True:
+        return("Must input integer")
+    
+    # check to make sure z is not zero or negative
+    if z <= 0:
+        return "The number of clubs to display must be a postive integer"
     
     conn = psycopg2.connect(host="localhost", 
                     port="5432", 
@@ -115,7 +151,11 @@ def num_clubs_lim(z):
     cur.close()
     conn.close()
 
-num_clubs_lim(5)
+num_clubs_lim(0)
+
+
+# In[89]:
+
 
 # question 4
 # What is the most popular nation_position and team_position in the dataset? (list the most popular for each)
@@ -141,7 +181,11 @@ def team_position_mode():
     cur.close()
     conn.close()
 
-team_position_mode()
+team_position_mode()[0]
+
+
+# In[55]:
+
 
 # question 4 continued
 def nation_position_mode():
@@ -166,6 +210,10 @@ def nation_position_mode():
     conn.close()
 
 nation_position_mode()
+
+
+# In[56]:
+
 
 # question 5
 # What is the most popular nationality for the players in the dataset?
@@ -194,5 +242,62 @@ def nationality_mode():
 nationality_mode()
 
 
+# In[ ]:
 
+
+# unit testing for each function
+
+import pytest
+from q1_src import *
+
+def test_most_improvement():
+    assert most_improvement(10) is not None, "Most improvement dataframe should not be empty"
+    assert most_improvement(10)[1] == [233727, 'Connor McLennan', 51, 67, 16], "Second most improved player should be Connor McLennan"
+    assert len(most_improvement(5)) == 5, "There should be 5 players"
+    assert most_improvement(5)[1] != [193512, 'Milan Heča', 58, 73, 15], "Second most improved player should not be Milan Heča"
+
+import pytest
+from q2_src import *
+
+def test_num_clubs():
+    assert num_clubs(10) is not None, "Number of clubs should not be empty"
+    assert len(num_clubs(5)) == 5, "Length of list should be 5"
+    assert num_clubs(10)[0] == (18, '1. FC Kaiserslautern'), "Club with most players with contracts ending in 2021 should be 1. FC Kaiserslautern"
+    assert num_clubs(10)[0] != (18, 'FC Ingolstadt 04'), "Club with most players with contracts ending in 2021 should not be FC Ingolstadt 04"
+    assert num_clubs(5)[1][0] > num_clubs(5)[2][0], "FC Ingolstadt 04 should have more players with contracts ending in 2021 than FC Girondins de Bordeaux does"
+    
+import pytest
+from q3_src import *
+
+def test_num_clubs_lim():
+    assert num_clubs_lim(10) is not None, "Number of clubs should not be empty"
+    assert num_clubs_lim(10)[0] == (33, 'Parma'), "Second element in list should be (33,'Parma')"
+    assert len(num_clubs_lim(5)) == 5, "There should be 5 clubs"
+    assert num_clubs_lim(5)[1] != (33, 'Parma'), "Second element in list should not be (33, 'Parma'))"
+    assert num_clubs_lim(5)[1][0] == num_clubs(5)[2][0], "Parma and Athletic Club de Bilbao should have the same number of players"
+    assert num_clubs_lim(3) is 'The number of clubs to display should at least be 5', "Should return an error message where z must be greater than equal to 5"
+    
+import pytest
+from q4_src import *
+
+def test_team_position():
+    assert team_position_mode() is not None, "Should not be empty and return positions"
+    assert len(team_position_mode()) == 1, "There should be one team position that is the most popular"
+    assert team_position_mode()[0][0] == 'GK', "The most popular team position should be GK, or goalkeeper"
+    assert team_position_mode()[0] == ('GK', 662), "The most popular team position should be GK, with 662 players with that position."
+
+def test_nation_position():
+    assert nation_position_mode() is not None, "Should not be empty and return positions"
+    assert len(nation_position_mode()) == 3, "There should be 3 most popular nation positions"
+    assert nation_position_mode()[0][0] == 'LCB', "The most popular team position should be GK, or goalkeeper"
+    assert nation_position_mode()[0] == ('LCB', 49), "The most popular team position should be GK, with 662 players with that position."
+    
+import pytest
+from q5_src import *
+
+def test_nationality():
+    assert nationality_mode() is not None, "Should return a nationality and not be empty"
+    assert nationality_mode()[0] == ('England', 1667), "The most popular nationality should be English"
+    assert len(num_clubs_lim()) == 1, "There should only be one most popular nationality"
+    assert nationality_mode()[0][0] == 'England', "Should return England"
 
